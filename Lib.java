29aaa8b0627eb5d05668bf8aa93273007ff2bbfb
henry1989 <enriquecuevas1989@gmail.com>
2016-03-15 11:15:50 +0100

    cambios en metodo3

--- Lib.java
+++ Lib.java
@@ -12,6 +12,9 @@ public class Lib {
     public void metodoAlumno2(){
         System.out.println("metodo 2");
     }
+        public void metodoAlumno3(){
+        System.out.println("metodo 3");
+    }
 
     public void metodoComunitario(){
         System.out.println("Aqui escribimos todos");
